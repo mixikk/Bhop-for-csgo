8c17dd57b57f0e955a7f3c8a62b291c076fb71d6
Maxmilian Matejka <Mixis97@seznam.cz>
2019-02-10 09:52:22 +0100

    Add files via upload

---
+++ Classes/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bhop.Classes
+{
+    public class Settings
+    {
+        public bool BunnyhopEnabled;
+        public bool ESP;
+    }
+}
